debc8a09e2c3fe688255eef23587da3f4c0caf88
alextoughg <atoxtough@gmail.com>
2013-05-27 21:59:28 -0400

    Started on the implementation of the dynamic array in C++.

--- dynamic_array.cpp
+++ dynamic_array.cpp
@@ -1,1 +1,15 @@
 using namespace std;
+
+struct dynarray{
+	// Content of dynamic array.
+	void * content;
+	// Logical size: number of elements 
+	// used by array.
+	int size;
+	// Physical size: maximum possible size 
+	// without relocating data.
+	int capacity; 
+	dynarray(){
+		;
+	}
+}
